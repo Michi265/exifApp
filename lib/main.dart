8e71f5feec1e8bb94fd0045bfc07bf32c333e02a
Michi265 <michela.crulli5@gmail.com>
2020-04-12 10:14:55 +0200

    add apk

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AgeMob User',
+      title: 'Exif Image Viewer',
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
